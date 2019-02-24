cb51bb363f41f936406aeac5db314a2576483faa
rasc0l <colin.r.knox@gmail.com>
2019-02-23 21:48:10 -0600

    clean up root test script and streamline logic

--- test.py
+++ test.py
@@ -3,14 +3,22 @@ import sys
 import subprocess
 import pkgutil
 
-if __name__ == '__main__':
-    test_args = 'python -m unittest'.split(' ')
-    module_names = []
 
-    if len(sys.argv) > 1:
-        module_names = sys.argv[1:]
-    else:
-        module_names = [name for _, name, _ in pkgutil.iter_modules(['test'])]
+test_args = 'python -m unittest'.split(' ')
+input_modules = []
+module_names = []
 
-    for module in module_names:
-        subprocess.run(test_args + [f'test.{module}'])
+if len(sys.argv) > 1:
+    input_modules = sys.argv[1:]
+
+module_names = [name for _, name, _ in pkgutil.iter_modules(['test'])]
+
+if input_modules:
+    module_names = set(input_modules) & set(module_names)
+
+if not module_names:
+    err_string = 'No valid test file supplied.\nTry calling with no arguments to run all tests or specify an existing test module.'
+    raise ValueError(err_string)
+
+for module in module_names:
+    subprocess.run(test_args + [f'test.{module}'])
